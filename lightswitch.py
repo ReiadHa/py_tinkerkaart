e011a2a57772f31b002fa780bee53fabe716cd6d
Riad Hak <99054420@mydavinci.nl>
2022-02-11 13:58:50 +0100

    opdracht

--- lightswitch.py
+++ lightswitch.py
@@ -1,13 +1,25 @@
 import tkinter as tk
 window = tk.Tk()
 
-button = tk.Button(text='...', bg="white", fg="black")
-button.pack(pady = 20, padx = 20)
+
+button = tk.Button(text='Hello World', bg="black", fg="white")
+button.pack(pady = 200, padx = 200)
 
 # schijf hier tussen je code
-
-
-
+getal = 0
+def functie():
+    global button,getal
+    if getal == 0:
+        print('test1')
+        window.config(bg='red')
+        button = tk.Button(text='Hello World', bg="black", fg="white")
+        getal += 1
+    elif getal == 1:        
+        print('test2')
+        window.config(bg='green')
+        button = tk.Button(text='Hello World', bg="white", fg="black")
+        getal -= 1
+button.config(command=functie)
 # schijf hier tussen je code
 
 window.mainloop()
